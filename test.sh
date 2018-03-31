f7e92d836869ba27b142c26a9abf61290e641db1
mgh3326 <mgh3326@naver.com>
2018-03-31 20:47:32 +0900

    update

--- test.sh
+++ test.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+if [ $# -eq 1 ]
+then # 매개 변수가 한개 일때 (띄어쓰기가 없는 경우)
 gcc "$1"
-
 ./a.exe
+elif [ $# -gt 1 ] # 띄어쓰기 있는 경우
+then
+    #     for a
+    # do
+    #     # echo $a
+    #     oh=$oh" "$a
+    # done
+    # git commit -m "$oh"
+else #매개 변수가 없는 경우 update 내용으로 commit 되도록 하였습니다.
+    # git commit -m "update"
+fi
+# git push origin master
